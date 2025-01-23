3de56f7105d0c5dee1eb5de159d373abf657797a
Gaurav Munjal <gaurav.munjal@civicplus.com>
2025-01-22 23:40:40 -0500

    do not drawPreviews after searching

--- sources/chargen.js
+++ sources/chargen.js
@@ -139,9 +139,7 @@ $(document).ready(function () {
       let parents = results.parents("ul");
       parents.prev("span").addClass("expanded").removeClass("condensed");
       for (const parent of parents.toArray().reverse()) {
-        $(parent).delay(50).show().map((i, el) => {
-          setTimeout(() => drawPreviews.call(el), 50 * i);
-        });
+        $(parent).delay(50).show();
       }
     }
   }
@@ -967,9 +965,9 @@ $(document).ready(function () {
       await new Promise((resolve) => {
         img.addEventListener('load', function () {
           callback(layers, prevctx);
-          resolve();
+          setTimeout(resolve, 4);
         });
-        img.addEventListener('error', resolve);
+        img.addEventListener('error', () => setTimeout(resolve, 50));
       });
       return img;
     }
