e5a3bb83c4e2dbc9637f521779cd0d2a3a78285c
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-02-04 11:53:10 -0500

    add tests for downloadFile

--- tests/tests.js
+++ tests/tests.js
@@ -6,6 +6,7 @@ config.truncateThreshold = 0; // Disable truncation of assertion errors
 // Import all test files
 import "./canvas/canvas-utils_spec.js";
 import "./canvas/draw-frames_spec.js";
+import "./canvas/download_spec.js";
 import "./components/CollapsibleSection_spec.js";
 import "./components/FiltersPanel_spec.js";
 import "./components/filters/AnimationFilters_spec.js";
