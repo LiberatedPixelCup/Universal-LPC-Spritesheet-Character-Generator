4a0a86e7515d0a04544c1b2377654b5d27485d6b
weiwentao996 <weiruowen@outlook.com>
2026-05-09 09:24:37 +0800

    feat: modify language selector location

--- sources/components/LanguageSelector.js
+++ sources/components/LanguageSelector.js
@@ -4,33 +4,20 @@ import { setAppLocale, state } from "../state/state.ts";
 
 export const LanguageSelector = {
   view: function () {
-    return m("div.box.mb-4.has-background-light", [
-      m("div.field.is-horizontal.is-align-items-center", [
-        m("div.field-label.is-normal", [
-          m(
-            "label.label.mb-0",
-            { for: "language-selector" },
-            t("language.label"),
+    return m("div.language-selector", [
+      m("label.is-sr-only", { for: "language-selector" }, t("language.label")),
+      m("div.select.is-small", [
+        m(
+          "select#language-selector",
+          {
+            value: state.locale,
+            title: t("language.label"),
+            onchange: (e) => setAppLocale(e.target.value),
+          },
+          LOCALE_OPTIONS.map((locale) =>
+            m("option", { value: locale.value }, locale.label),
           ),
-        ]),
-        m("div.field-body", [
-          m("div.field.mb-0", [
-            m("div.control", [
-              m("div.select.is-small", [
-                m(
-                  "select#language-selector",
-                  {
-                    value: state.locale,
-                    onchange: (e) => setAppLocale(e.target.value),
-                  },
-                  LOCALE_OPTIONS.map((locale) =>
-                    m("option", { value: locale.value }, locale.label),
-                  ),
-                ),
-              ]),
-            ]),
-          ]),
-        ]),
+        ),
       ]),
     ]);
   },
