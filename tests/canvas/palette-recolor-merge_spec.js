474594dcf26251824bcfd0bb742bc71fb4e0a5b0
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-08-03 11:42:12 -0400

    Add initial WebGL smoke test

--- tests/canvas/palette-recolor-merge_spec.js
+++ tests/canvas/palette-recolor-merge_spec.js
@@ -15,34 +15,11 @@ import {
   getPaletteRecolorConfig,
 } from "../../sources/canvas/palette-recolor.ts";
 import { getPaletteMetadata } from "../../sources/state/catalog.ts";
-
-function solidCanvas(r, g, b, w = 4, h = 4) {
-  const c = document.createElement("canvas");
-  c.width = w;
-  c.height = h;
-  const ctx = c.getContext("2d");
-  ctx.fillStyle = `rgb(${r},${g},${b})`;
-  ctx.fillRect(0, 0, w, h);
-  return c;
-}
-
-/** Two-color 4x4 canvas: left half color A, right half color B. */
-function splitCanvas(a, b) {
-  const c = document.createElement("canvas");
-  c.width = 4;
-  c.height = 4;
-  const ctx = c.getContext("2d");
-  ctx.fillStyle = `rgb(${a.r},${a.g},${a.b})`;
-  ctx.fillRect(0, 0, 2, 4);
-  ctx.fillStyle = `rgb(${b.r},${b.g},${b.b})`;
-  ctx.fillRect(2, 0, 2, 4);
-  return c;
-}
-
-function readPixel(canvas, x, y) {
-  const data = canvas.getContext("2d").getImageData(x, y, 1, 1).data;
-  return { r: data[0], g: data[1], b: data[2], a: data[3] };
-}
+import {
+  solidCanvas,
+  splitCanvas,
+  readPixel,
+} from "./palette-recolor-test-helpers.js";
 
 describe("canvas/palette-recolor.ts single-pass merge (CPU path)", () => {
   let previousMode;
